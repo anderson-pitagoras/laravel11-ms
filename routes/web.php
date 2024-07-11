c98059a3d960f9679e9e3712821d85c7529856e3
Anderson Pitagoras <anderson.pitagoras@gmail.com>
2024-07-10 21:48:29 -0300

    router

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Ms\MsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clientes', [MsController::class, 'clientes']);
+
+
